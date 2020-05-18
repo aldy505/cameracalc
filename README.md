3baec31fa87f2b5467835030dfd9528dfef1edaf
aldy505 <reinaldyrafli@gmail.com>
2020-05-18 11:44:21 +0700

    nuxt js folder

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This v2.0 is in production. Here is what coming so far:
 
- - Re-written in PHP, Materialize, and VueJS
+ - Re-written in NuxtJS (for SEO and easy routing)
  - Updated UI, much simpler and cleaner
- - New light data
+ - New light data (but not all of them are completed yet)
+ - JSON as database holder (db.json)
+
+I don't have all the time in the world. I will be working on this project on my spare time (from works and college).
---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'title', content: 'Camera Calculator' },
+      { name: 'description', content: 'The online calculator on camera settings and lighting intensity.' },
+      { name: 'keywords', content: 'calculator, film, camera, lighting, iso, aperture' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'language', content: 'English' },
+      { name: 'author', content: 'Reinaldy Rafli' },
+      { name: 'revisit-after', content: '14 days' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
